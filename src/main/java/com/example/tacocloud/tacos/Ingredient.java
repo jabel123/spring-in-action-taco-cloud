ffc30c4446303761000b325e9a23067c0d17c7e9
juhyeontae <jabel123@naver.com>
2022-01-17 07:34:19 +0900

    chap03 : JPA
    JPA  엔티티 설정중. 왜 특정 Entity에만 Serializable이 구현체로 설정되어있는가..?

--- src/main/java/com/example/tacocloud/tacos/Ingredient.java
+++ src/main/java/com/example/tacocloud/tacos/Ingredient.java
@@ -1,12 +1,20 @@
 package com.example.tacocloud.tacos;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity
+@NoArgsConstructor(force = true)
 @RequiredArgsConstructor
 public class Ingredient
 {
+    @Id
     private final String id;
     private final String name;
     private final Type type;
